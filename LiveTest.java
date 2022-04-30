4f2e6932bd0613090a08d9f4e1652cf88693fa88
JasonMorris1 <jasonbm123@gmail.com>
2022-04-30 16:34:06 -0700

    added code

--- LiveTest.java
+++ LiveTest.java
@@ -1,1 +1,19 @@
 
+import org.junit.*;
+import static org.junit.Assert.*;
+public class LiveTest
+{
+    @Test
+    public void testTwoSum(){
+
+        int[] input  ={ 2, 7 ,11, 15};
+        int[] expected = {0 , 1};
+        int target = 12;
+
+        int[] output = Live.twoSum(input, target);
+
+        assertArrayEquals(expected, output);
+
+    }
+
+}
